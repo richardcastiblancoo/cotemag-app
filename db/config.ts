8f1abbcd8c6d37d7740b42b41e8b30db5545f64d
richardcastiblancoo <castiblancorichard8@email.com>
2025-03-09 20:51:42 -0500

    agregando error index

--- db/config.ts
+++ db/config.ts
@@ -2,31 +2,6 @@ import { defineDb, defineTable, column } from 'astro:db';
 
 // https://astro.build/db/config
 
-const Administrador = defineTable({
-  columns: {
-    id: column.text({primaryKey:true}),
-    email: column.text({unique:true}),
-    username: column.text({unique:true}),
-});
-
-const Estudiantes = defineTable({
-  columns:{
-    id: column.text({primaryKey:true}),
-    identidad: column.text({unique:true}),
-    nombre: column.text(),
-    apellido: column.text(),
-    email: column.text({unique:true}),
-    telefono: column.text(),
-    direccion: column.text(),
-    fecha_nacimiento: column.text(),
-    genero: column.text(),
-    estado: column.text(),
-    id_carrera: column.text(),
-    id_facultad: column.text(),
-    id_universidad: column.text(),
-    password: column.text(),
-});
-
 
 export default defineDb({
   tables: {}
