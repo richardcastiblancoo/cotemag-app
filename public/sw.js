375ce92daa93b4d4faf88a7bb5b4f95621909fd3
richardcastiblancoo <castiblancorichard8@email.com>
2025-03-11 15:19:12 -0500

    agregando nuevo contenido por ejemplo pwa google analytyc y mejorando el rendimiento

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", (event) => {
+    console.log("Service Worker instalado");
+    self.skipWaiting();
+  });
+  
+  self.addEventListener("activate", (event) => {
+    console.log("Service Worker activado");
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    console.log("Interceptando petición a:", event.request.url);
+  });
+  
